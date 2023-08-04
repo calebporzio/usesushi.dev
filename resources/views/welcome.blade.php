b377cc19850832e749d950f60b02b281b1c47235
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-08-04 17:03:00 +0330

    Add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,16 +13,62 @@
 
         <title>Sushi - Eloquent’s missing “array” driver.</title>
 
+        {{-- Alpine Style --}}
         <style>
             [x-cloak] {
                 display: none !important;
             }
         </style>
 
+        {{-- Favicon --}}
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png?v=93mfAPBMrCx"
+        />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png?v=93mfAPBMrCx"
+        />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png?v=93mfAPBMrCx"
+        />
+        <link
+            rel="manifest"
+            href="/favicon/site.webmanifest?v=93mfAPBMrCx"
+        />
+        <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg?v=93mfAPBMrCx"
+            color="#5bbad5"
+        />
+        <link
+            rel="shortcut icon"
+            href="/favicon/favicon.ico?v=93mfAPBMrCx"
+        />
+        <meta
+            name="msapplication-TileColor"
+            content="#b91d47"
+        />
+        <meta
+            name="msapplication-config"
+            content="/favicon/browserconfig.xml?v=93mfAPBMrCx"
+        />
+        <meta
+            name="theme-color"
+            content="#fff0e7"
+        />
+
+        {{-- Vite --}}
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body
-        class="overflow-x-hidden bg-[#FFF0E7] font-poppins text-stone-900 antialiased selection:bg-stone-500/10 pb-14"
+        class="overflow-x-hidden bg-[#FFF0E7] pb-14 font-poppins text-stone-900 antialiased selection:bg-stone-500/10"
     >
         <x-hero />
         <x-quick-start />
